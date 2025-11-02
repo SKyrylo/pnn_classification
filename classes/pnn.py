d21f06665a441a24f0e49919a832316fe9358bff
Kyrylo <kyrylo.svystun@litslink.com>
2025-11-02 11:24:51 +0200

    Working system commit

---
+++ classes/pnn.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import numpy as np
+
+
+class PNN:
+    def __init__(self, df: pd.DataFrame):
+        """
+        DataFrame info:
+            1. Must contain target column named "target"
+            2. All other columns are considered features
+        """
+        assert "target" in df.columns, "No target column named 'target' found!"
+        assert len(df.columns) > 1, "Not enough feature columns in dataframe!"
+
+        df = df.copy()
+        self.target = df.pop("target")
+        self.features = df
+
+        check = [df[f'{col}'].dtype in ("float64", "int64") for col in df.columns]
+        assert all(check), "All feature columns must be type 'float64' or 'int64'!"
+        
+        self.num_features = len(self.features.columns)
+        self.classes = self.target.unique()
+
+        self.input_layer = InputLayer(self.num_features)
+        self.pattern_layer = PatternLayer(df)
+        self.summation_layer = SummationLayer(self.target)
+        self.decision_layer = DecisionLayer()
+    
+    def __call__(self, features: np.ndarray) -> str:
+        pattern_out = self.pattern_layer(self.input_layer(features))
+        summation_out = self.summation_layer(pattern_out, self.target)
+        decision_out = self.decision_layer(summation_out)
+
+        return decision_out
+
+class InputLayer:
+    def __init__(self, num_features: int):
+        self.num_features = num_features
+    
+    def __call__(self, features):
+        assert len(features) == self.num_features, f"Number of training features != {self.num_features}"
+        return features
+
+
+class PatternLayer:
+    def __init__(self, df):
+        self.num_features = len(df.columns)
+        self.num_samples = len(df)
+
+        self.weights = []
+        for i in range(self.num_samples):
+            self.weights.append(df.iloc[i].values)
+        
+        self.weights = np.array(self.weights)
+    
+    def __call__(self, features: np.ndarray):
+        shape = (self.num_features,)
+        assert features.shape == shape, f"Feature array must be of shape {shape}!"
+
+        outs = []
+        for i in range(len(self.weights)):
+            outs.append(np.exp(-np.sum((self.weights[i]-features)**2)))
+
+        return np.array([outs]).T
+
+
+class SummationLayer:
+    def __init__(self, num_classes):
+        self.num_classes = num_classes
+
+    def __call__(self, inputs, target):
+        df = target.to_frame()
+        df['pattern_outs'] = inputs
+
+        outs = df.groupby("target").sum()
+        return outs
+
+
+class DecisionLayer:
+    def __init__(self):
+        pass
+    
+    def __call__(self, summation_out: pd.DataFrame):
+        highest_scoring_class_idx = np.argmax(summation_out)
+        classified_class = summation_out.index[highest_scoring_class_idx]
+
+        return classified_class
---
+++ main.py
@@ -0,0 +1,52 @@
+﻿from classes.pnn import PNN
+import pandas as pd
+import numpy as np
+
+
+def get_accuracy(preds: list[str], gt: list[str]):
+    bools = [pred == ground for pred, ground in zip(preds, gt)]
+    return sum(bools) / len(bools)
+
+
+def main():
+    print("===== PNN for classification of samples with N features =====")
+
+    # Training data for testing purposes
+    df = pd.DataFrame(
+        data=[[1,2, 'A'], [2,1, 'A'], [10,20,'B'], [20,10,'B']],
+        columns=['x', 'x2', 'target']
+    )
+
+    # ----- Initialize PNN model (trained when initialized) -----
+    pnn = PNN(df)
+
+    # ----- Validate model to see accuracy -----
+    preds, gt = [], df.target.to_list()
+    print("\n----- Validate model on training dataset -----")
+
+    for i in range(len(df)):
+        sample = np.array(df.iloc[i].drop('target').values)
+        pred = pnn(sample)
+        preds.append(pred)
+        print(f"\t~ Sample - {sample}, Prediction - {pred}, True - {df.iloc[i].target}")
+    print(f"Overall Accuracy: {get_accuracy(preds, gt)}")
+
+    # ----- Test model on unseen data -----
+    test_df = pd.DataFrame(
+        data=[[3,4,'A'], [8,9,'A'], [16,11,'B'], [25,30,'B'], [0,0,'A']],
+        columns=['x1','x2','target']
+    )
+
+    preds, gt = [], test_df.target.to_list()
+    print("\n----- Test model on unseen data -----")
+
+    for i in range(len(test_df)):
+        sample = np.array(test_df.iloc[i].drop('target').values)
+        pred = pnn(sample)
+        preds.append(pred)
+        print(f"\t~ Sample - {sample}, Prediction - {pred}, True - {test_df.iloc[i].target}")
+    print(f"Overall Accuracy: {get_accuracy(preds, gt)}")
+
+
+if __name__ == "__main__":
+    main()
